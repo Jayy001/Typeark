d298dc7f982080b1d6d1015f533174ad0ffc85d0
Jayy001 <jayy2004@gmx.co.uk>
2022-12-10 22:25:50 +0000

    Cleaned up

--- typeark/__main__.py
+++ typeark/__main__.py
@@ -7,7 +7,10 @@ def run():
     if cfg:
         fd = FontDictionary(cfg)
         fd.save_json_dict()
-
+        
+    fr = FileRemote()
+    fr.inject_contents()
+    
 if __name__ == '__main__':
     run()
     
